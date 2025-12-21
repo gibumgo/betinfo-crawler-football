ab12bfa16c294f9782b9dfaa1751a85938007945
gibumgo <gibumgo@gmail.com>
2025-12-21 16:57:56 +0900

    feat: 브라우저 뷰포트 크기 고정 기능 추가

--- driver/chrome_driver_factory.py
+++ driver/chrome_driver_factory.py
@@ -8,4 +8,5 @@ class ChromeDriverFactory:
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
         return webdriver.Chrome(options=options)
