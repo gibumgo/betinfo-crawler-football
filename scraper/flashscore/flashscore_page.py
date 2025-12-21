53ba373af9f43a0c3950dcc506b9aa4d48f03d0f
gibumgo <gibumgo@gmail.com>
2025-12-21 14:21:37 +0900

    refactor: BaseScraper 예외처리 출력문 추가, init 파일 삭제

--- scraper/flashscore/flashscore_page.py
+++ scraper/flashscore/flashscore_page.py
@@ -6,18 +6,6 @@ class FlashscorePage(BaseScraper):
     def open(self):
         self.open_url(self.BASE_URL)
 
-    def goto_league_standings(self, league_url_path: str):
-        path = league_url_path.rstrip('/')
-        full_url = f"{self.BASE_URL}{path}/standings/"
-        self.open_url(full_url)
-        self.wait_for_element(".tableCellParticipant__name")
-
-    def goto_team_details(self, team_url_path: str):
-        path = team_url_path.rstrip('/')
-        full_url = f"{self.BASE_URL}{path}/"
-        self.open_url(full_url)
-        self.wait_for_element(".teamHeader__name")
-
     def goto_match_results(self, league_url_path: str):
         path = league_url_path.rstrip('/')
         full_url = f"{self.BASE_URL}{path}/results/"
@@ -29,13 +17,3 @@ class FlashscorePage(BaseScraper):
         full_url = f"{self.BASE_URL}{path}/fixtures/"
         self.open_url(full_url)
         self.wait_for_element(".event__match")
-
-    def click_show_more(self) -> bool:
-        """'더 보기' 버튼이 있으면 클릭합니다."""
-        try:
-            from selenium.webdriver.common.by import By
-            show_more_button = self.driver.find_element(By.CSS_SELECTOR, ".event__more")
-            self.driver.execute_script("arguments[0].click();", show_more_button)
-            return True
-        except:
-            return False
